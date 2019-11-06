2c49131a4317c78fd752ab08c5ee13bbffefe2e1
Javier Collipal <jcollipal1212@gmail.com>
2019-11-06 17:25:55 -0300

    smarcheckout logic added, time to link it with rabbitqm
    Took 1 hour 6 minutes

--- src/amqp/smartCheckout.js
+++ src/amqp/smartCheckout.js
@@ -1,16 +1,35 @@
-const { MercadoPago, defaultPreferenceMaker } = require("../config/mercadoPago/mercadoPago");
+const {MercadoPago, defaultPreferenceMaker} = require("../config/mercadoPago/mercadoPago");
 const itemsMaker = require("../common/mercadopago/items");
 const onErr = require('../common/onErr');
 //nest api job is getting all the items data so the microservice is only going to deal
 //with mercadopago Items,Payer and Preferences interface.
 
-const itemsHandler = (products) => itemsMaker(products);
-const preferencesHandler = (items) => defaultPreferenceMaker(items);
+const smartCheckoutHandler = (preferences) => {
+    return new Promise(((resolve, reject) => {
+        MercadoPago.preferences.create(preferences).then(result => resolve(result)).catch(e => reject(e))
+    }));
+};
+
+const transactionHandler = (items , userId, preferenceId, state) => {
+    console.log(items);
+    console.log(userId);
+    console.log(preferenceId);
+    console.log(state);
+};
+const initPointHandler  = (initPoint) => initPoint;
 
 const msgHandler = (msg) => {
-
+    const message = JSON.parse(msg);
+    const items = itemsMaker(message.products);
+    const preferences = defaultPreferenceMaker(items);
+    const responseFromMercadoPago = smartCheckoutHandler(preferences);
+    responseFromMercadoPago
+        .then(res => {
+            transactionHandler(res.body.items, res.body.userId, res.body.id);
+            return initPointHandler(res.body.init_point);
+        })
+        .catch(onErr)
 };
+
 /*ESTA PARTE HACERLA PROMISE O VER SI EL THENABLE NOS DEVUELVE EL BODY.INIT*/
-MercadoPago.preferences.create(preferences).then(resultado => {
-    console.log(resultado.body.init_point);
-}).catch(onErr);
+
