90abace33fe1b03c3d51364056da960a5a260609
jcollipal <jcollipal1212@gmail.com>
2019-11-18 16:37:57 -0300

    request fixed#
    Took 4 minutes

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -4,9 +4,8 @@ const { dataResponse } = require("../common/response");
 const paymentController = {
   handleMercadoPagoNotification(req, res) {
     console.log(req.query);
-    paymentService.handleMercadoPagoNotification(req.query.id || req.query.data.id,
-      req.query.topic || req.query.type, req.body);
     res.sendStatus(201);
+    paymentService.handleMercadoPagoNotification(req.query.id,req.query.topic || req.query.type, req.body);
   },
 };
 
