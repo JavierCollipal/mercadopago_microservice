e34aa7e7156d2fe8662cb8be959a8026350c0775
jcollipal <jcollipal1212@gmail.com>
2019-11-19 12:36:34 -0300

    testing new changes in user transaction model
    Took 1 minute

--- src/models/users_transactions.js
+++ src/models/users_transactions.js
@@ -16,6 +16,11 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      companyUserId: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
     },
   );
 };
