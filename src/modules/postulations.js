f47ff962dd518ec595fdaeb237916b2191324996
jcollipal <jcollipal1212@gmail.com>
2019-11-21 19:31:59 -0300

    testing new postulation status

--- src/modules/postulations.js
+++ src/modules/postulations.js
@@ -1,6 +1,7 @@
 const { postulationModel } = require("../database/index");
-
+const  { logger } = require("../config/logger/pino");
 const updatePostulationState = (postulationId, paymentStatus) => {
+  logger.info("nuevo estado del pago en la postulacion: "+paymentStatus);
   postulationModel.update({ payStatus: paymentStatus }, { where: { id: postulationId } });
 };
 
